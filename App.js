476366554b65c5e5401dbfc83c7e5e0d3e522c6e
rahulsharma143 <49840924+rahulsharma143@users.noreply.github.com>
2020-10-08 11:41:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React , {Component} from 'react';
+import BurgerBuilder from'./containers /BurgerBuilder'; 
+
+import './App.css';
+import Layout from './components/Layout/Layout';
+
+class App extends Component  {
+render(){
+  return(
+    <div>
+       <Layout>
+        <BurgerBuilder/>
+       </Layout>
+    </div>
+
+  );
+}
+}
+
+export default App;
